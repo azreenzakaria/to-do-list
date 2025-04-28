6fa7933c721482f0d6c6c1b8ebe5594cb718b86a
azreenzakaria <mazreen.zakaria@gmail.com>
2025-04-28 23:57:40 +0800

    Fix error message

--- src/constants.ts
+++ src/constants.ts
@@ -4,6 +4,6 @@ export const API_RESPONSE_MESSAGE = {
   passwordNotValid:
     'The password must be at least 8 characters long and a combination of uppercase letters, lowercase letters, numbers, and symbols',
   passwordNotMatch: 'Password does not match',
-  userNotRegistered: 'Please Register First',
+  userNotRegistered: 'Please register an account to continue.',
   userAlreadyRegistered: 'User already registered. Please sign in.',
 };
